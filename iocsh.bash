d42523f07aff159c108157b92d7e74cb4a9eaa77
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-10-09 21:25:11 +0200

    add update iocsh.bash for clarity

--- iocsh.bash
+++ iocsh.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 declare -gr SC_SCRIPT="$(realpath "$0")"
 declare -gr SC_SCRIPTNAME=${0##*/}
 declare -gr SC_TOP="$(dirname "$SC_SCRIPT")"
@@ -12,7 +11,6 @@ set -a
 set +a
 
 . ${SC_TOP}/iocsh_functions
-
 
 
 case $1 in
@@ -29,64 +27,34 @@ case $1 in
     ;;
 esac
 
-# Default, use the default EPICS_BASE in env setup script
-# echo ${EPICS_BASE}
-# What about EPICS_BASE exists, however, require doesn't compile with that base...?
-
-
 if [[ $(checkIfDir ${EPICS_BASE}) -eq "$NON_EXIST" ]]; then
 
-    printf "${EPICS_BASE} is defined to use. \n";
-    printf "HOWEVER, ${SC_SCRIPTNAME} cannot find the real directory at EPICS_BASE at ${EPICS_BASE}\n";
+    printf "EPICS_BASE is defined to use. \n";
     printf "Please check your environment!\n";
     exit;
 fi
 
 declare -g RUNNING_EPICS_BASE_VER=${EPICS_BASE##*/*-}
-declare -g RUNNING_REQUIRE_PATH=${EPICS_MODULES}/${REQUIRE}/${REQUIRE_VERSION}/R${RUNNING_EPICS_BASE_VER}
-
-declare -g SOFTIOC_CMD="softIoc"
-declare -g SOFTIOC_ARGS="-D ${EPICS_BASE}/dbd/softIoc.dbd"
+declare -g RUNNING_REQUIRE_PATH=${REQUIRE_PATH}/R${RUNNING_EPICS_BASE_VER}
 
 STARTUP=/tmp/${SC_SCRIPTNAME}_${IOC}_startup.$BASHPID
+
+declare -a ioc_env=(PWD EPICS_HOST_ARCH  REQUIRE_PATH EPICS_CA_ADDR_LIST);
 
 trap "softIoc_end" EXIT SIGTERM
 
 {
-    echo "# date=\"$(date)\""
-    echo "# user=\"${USER:-$(whoami)}\""
-    
-    for var in PWD EPICS_HOST_ARCH  REQUIRE_PATH EPICS_CA_ADDR_LIST
-    do
-	echo "# $var=\"${!var}\""
-    done
+    printIocEnv "${ioc_env}"
 
+    loadRequire 
+    loadFiles   "$@"
 
-    LIBPREFIX=lib
-    LIBPOSTFIX=.so
-
-    REQUIRE_LIB=${RUNNING_REQUIRE_PATH}/lib/${EPICS_HOST_ARCH}/${LIBPREFIX}${REQUIRE}${LIBPOSTFIX}
-    REQUIRE_DBD=${REQUIRE_LIB%/lib/*}/dbd/${REQUIRE}.dbd
-
-    LDCMD="dlload"
-
-    echo "$LDCMD $REQUIRE_LIB"
-    echo "dbLoadDatabase $REQUIRE_DBD"
-    echo "${REQUIRE%-*}_registerRecordDeviceDriver"
-
-    loadFiles "$@"
-
-    if [ "$init" != NO ]
-    then
+    if [ "$init" != NO ]; then
 	echo "iocInit"
     fi
 
     
-} > ${STARTUP}
+}  > ${STARTUP}
 
-
-    
-command='${SOFTIOC_CMD} ${SOFTIOC_ARGS} "${STARTUP}"'
 ulimit -c unlimited
-eval "${command}" 2>&1
-
+softIoc -D ${EPICS_BASE}/dbd/softIoc.dbd "${STARTUP}" 2>&1
